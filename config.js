044a8aa99efe8584bb4045d26c30223fba38bd92
sukkla01 <sukkla01@gmail.com>
2023-06-26 12:59:29 +0700

    new

--- config.js
+++ config.js
@@ -1,22 +1,22 @@
-// module.exports = {
-//     secret: '1234',
-//     dbOption: {
-//         host: 'localhost',
-//         user: 'root',
-//         password: '',
-//         database: 'ssw_run',
-//         port: '3306'
-//     }
-// }
 module.exports = {
     secret: '1234',
     dbOption: {
-        host: '110.49.126.23',
-        user: 'ict009',
-        password: 'Ss0810432245',
+        host: 'localhost',
+        user: 'root',
+        password: '',
         database: 'plan',
-        port: '3306',
-        charset: 'utf8mb4'
-    },
-    // token_group_line :'tsC5SYKialkyXj3UDrnwA8ck3FSk9qDKEQXBzdTyRzo'
+        port: '3306'
+    }
 }
+// module.exports = {
+//     secret: '1234',
+//     dbOption: {
+//         host: '110.49.126.23',
+//         user: 'ict009',
+//         password: 'Ss0810432245',
+//         database: 'plan',
+//         port: '3306',
+//         charset: 'utf8mb4'
+//     },
+//     // token_group_line :'tsC5SYKialkyXj3UDrnwA8ck3FSk9qDKEQXBzdTyRzo'
+// }
