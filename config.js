e33299f609b65ba480affd76811b31765b71966c
sukkla01 <sukkla01@gmail.com>
2026-09-21 10:34:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -8,15 +8,3 @@ module.exports = {
         port: '3306'
     }
 }
-// module.exports = {
-//     secret: '1234',
-//     dbOption: {
-//         host: '110.49.126.23',
-//         user: 'ict009',
-//         password: 'Ss0810432245',
-//         database: 'plan',
-//         port: '3306',
-//         charset: 'utf8mb4'
-//     },
-//     // token_group_line :'tsC5SYKialkyXj3UDrnwA8ck3FSk9qDKEQXBzdTyRzo'
-// }
